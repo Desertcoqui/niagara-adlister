5844eb9e80468c466c8cdba7e4417f57a4b205b4
PapoteDetres <desertcoqui@yahoo.com>
2017-02-01 14:39:29 -0600

    ListAdsDao errors fixed, parameters were off.

---
+++ resources/migration.sql
@@ -0,0 +1,7 @@
+use adlister_db;
+
+Insert into ads(title, description) VALUES 
+  ('Playstation for sale','broke'),
+  ('We have a few broken phones', 'broke'),
+  ('A nokia food truck', 'broke'),
+  ('Window washer for your glasses','broke');
---
+++ src/main/java/JdbcAdsDao.java
@@ -0,0 +1,45 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.List;
+
+/**
+ * Created by PapoteDetres1 on 2/1/17.
+ */
+public class JdbcAdsDao implements Ads{
+    private Connection connection;
+
+    public JdbcAdsDao() throws SQLException{
+        Driver driver = new Driver();
+
+        DriverManager.registerDriver(driver);
+
+        DriverManager.getConnection(
+                "jdbc:mysql://localhost/adlister_db",
+                "root",
+                "Codeup1!");
+    }
+
+    @Override
+    public List<Ad> all() {
+        String sql= "select * from ads";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("description");
+
+                Ad ad = new Ad(id, title, description);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
